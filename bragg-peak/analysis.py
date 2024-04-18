51287e2a4a246233990cf35647ef07f5d99b2bd6
Deepak Samuel <deepaksamuel@gmail.com>
2024-04-19 01:04:42 +0530

    --

---
+++ bragg-peak/analysis.py
@@ -0,0 +1,7 @@
+#%%
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+headers = ["binx","biny","binz",'eDep-MeV','total','entries']
+df = pd.read_csv("build/eDep.txt",skiprows = 3,header=None,names=headers)
+plt.scatter(df["binz"],df["eDep-MeV"]) 
