5f1d9d8f08a8326684d76090f747c87e93123770
Deepak Samuel <deepaksamuel@gmail.com>
2024-04-27 17:11:55 +0530

    --

--- alpha-scattering/analysis.py
+++ alpha-scattering/analysis.py
@@ -2,7 +2,7 @@
 import numpy as np 
 import matplotlib.pyplot as plt
 import pandas as pd
-# from scipy.optimize import curve_fit
+from scipy.optimize import curve_fit
 
 
 headers = ['eid','pid','detector','is_primary','x','y','z','del_x','del_y','del_z','vx','vy','vz','delt','globTime',"propTime",'localTime','ni_edep','tot_edep',
@@ -10,13 +10,8 @@ headers = ['eid','pid','detector','is_primary','x','y','z','del_x','del_y','del_
 
 df = pd.read_csv("build/output_nt_simple.csv",skiprows=30,header=0,names=headers)
 
-df2 = df[df["pid"]==1000020000]
 
-plt.hist(df['pid'],log=True) 
-
-# df['pid'].unique()
-
-# df = df[df['pid']==1000020000] # select alpha particles
+df = df[df['pid']==1000020000] # select alpha particles
 
 # plt.hist(df['z'],log=True) 
 
@@ -24,30 +19,35 @@ plt.hist(df['pid'],log=True)
 
 # plot angular distribution
 
-# r= np.sqrt(df['x']**2 + df['y']**2 + df['z']**2)  # Magnitude of the vector
-# zenith_rad = np.arccos(np.sqrt(df['x']**2 + df['z']**2) / r)    
-# plt.hist(np.degrees(zenith_rad),bins=100,log=True)
+r= np.sqrt(df['x']**2 + df['y']**2 + df['z']**2)  # Magnitude of the vector
+r2= np.sqrt(df['x']**2 + df['y']**2)  # Magnitude of the vector
+zenith_rad = np.arccos( df['z'] / r)   
+phi_rad =  np.sign(df['y']) *np.arccos( df['x'] / r2)   
+df['theta'] = zenith_rad
+df['phi'] = phi_rad
+
+
+plt.hist(np.degrees(df['theta']),bins=100,log=True)
+
 
 
 
 # fitting cosec^4(theta/2) !!! needs updating
 
 # def func(theta, a):
-#     return a * np.power(np.sin(theta /2), -4)
+#     return a * np.power(np.sin(theta),-4)
 
-# df = df[df['pid']==1000020000] # select alpha particles
 
-# r= np.sqrt(df['x']**2 + df['y']**2 + df['z']**2)  # Magnitude of the vector
-# zenith_rad = np.arccos(np.sqrt(df['x']**2 + df['z']**2) / r)
+# counts, bins, patches = plt.hist(zenith_rad , bins=25)
 
-# counts, bins, patches = plt.hist(zenith_rad , bins=25, density=True)
-# # plt.show()
 # bin_centers = (bins[:-1] + bins[1:]) / 2
 
 # # Fit the function to the histogram
-# popt, pcov = curve_fit(func, bin_centers, counts, p0=1)
+# popt, pcov = curve_fit(func, bin_centers, counts  )
 
 # a = popt[0]
 
-# plt.plot(bin_centers, func(bin_centers, a), label='Fitted curve')
+# plt.scatter(bin_centers, func(bin_centers, a), label='Fitted curve', color='orange')
+
+
 #%%
